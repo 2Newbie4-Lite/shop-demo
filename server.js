8693daa77a33447f600d583c06d17fa47901743c
2Newbie4-Lite <anhkodoiqua5b@gmail.com>
2026-03-09 15:18:44 +0700

    update port for deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cartRoutes = require('./routes/cartRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
